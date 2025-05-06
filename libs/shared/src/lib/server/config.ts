8971d8cd3c7b2935169c44264996a0023bcca34d
Codescype <engineer.codescype@gmail.com>
2025-05-06 04:25:31 +0100

    feat: add server configuration to the shared library

---
+++ libs/shared/src/lib/server/config.ts
@@ -0,0 +1,5 @@
+// Set the host name for the server
+export const hostname = process.env.SERVER_APP_HOST ?? '127.0.0.1';
+
+// Set the port for the server to listen on
+export const port = process.env.SERVER_APP_PORT ?? 3001;
