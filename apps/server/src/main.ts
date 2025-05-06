ddb48367f19b7513254a2d4952627c9977483870
Codescype <engineer.codescype@gmail.com>
2025-05-06 03:16:41 +0100

    feat: add Elysia server setup with CORS and Swagger integration
    - Implemented Elysia server in main.ts with CORS and Swagger middleware
    - Updated .gitignore to exclude .env files
    - Added new dependencies for Elysia framework in package.json
    - Updated pnpm-lock.yaml to reflect new dependencies
    - Added launch configuration for debugging the server in VSCode

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,34 @@
-console.log('Hello World');
+import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
+import { node } from '@elysiajs/node';
+import 'dotenv/config';
+
+// Setup the Elysia server
+const app = new Elysia({ adapter: node() });
+
+// Add CORS middleware to enable requests from different origins
+app.use(cors());
+
+// Add Swagger middleware for API documentation
+app.use(swagger());
+
+app
+  .onError(({ error }) => {
+    // @ts-expect-error Response is not a valid type
+    new Response(error.toString());
+  })
+  .get('/', () => 'Hello From the Server!');
+
+// Set the host name for the server
+const hostname = process.env.SERVER_APP_HOST ?? '127.0.0.1';
+// Set the port for the server to listen on
+const port = process.env.SERVER_APP_PORT ?? 3001;
+
+// listen for incoming requests
+app.listen({ hostname, port }, (server) => {
+  // Log the server URL to the console
+  console.log(`🦊 Server is running at ${server.hostname}:${server.port}`);
+});
+
+export default app;
