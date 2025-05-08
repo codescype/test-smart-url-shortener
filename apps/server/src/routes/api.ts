c65892253e7138842a6e41bd90c7c8f7c103d820
Codescype <engineer.codescype@gmail.com>
2025-05-08 06:17:09 +0100

    feat: implement API routes for URL encoding, decoding, and statistics

--- apps/server/src/routes/api.ts
+++ apps/server/src/routes/api.ts
@@ -1,21 +1,86 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
-const apiRoutes = new Elysia().group('/api', (app) => {
-  return app
-    .get(
-      '/encode/:originalURL',
-      ({ params }) => `Encode ${params.originalURL} on the API!`
-    )
-    .get(
-      '/decode/:encodedURL',
-      ({ params }) => `Decode ${params.encodedURL} on the API!`
-    )
-    .get(
-      '/statistic/:encodedURL',
-      ({ params }) => `Get statistic for ${params.encodedURL} on the API!`
-    )
-    .get('/:encodedURL', ({ params }) => `Redirect to ${params.encodedURL}`)
-    .get('/list', () => 'List all URLs from the API!');
-});
+import { ShortenedURLController } from './../controllers/ShortenedURL.controller.js';
+
+// Instantiate the URLShortenerService
+const shortenedURLController = new ShortenedURLController();
+
+const apiRoutes = new Elysia()
+  .onError(({ code, error }) => {
+    console.error('Error:', error);
+
+    return {
+      status: code,
+      message: error,
+    };
+  })
+  .group('/api', (app) => {
+    return app
+      .post(
+        '/encode',
+        ({ body, error }) => {
+          const shortenedURL = shortenedURLController.encode(body.url);
+
+          // Return 404 if the URL is not found
+          if (!shortenedURL) {
+            return error(404, 'URL not found');
+          }
+
+          // Return the shortened URL data as JSON
+          return { ...shortenedURL };
+        },
+        { body: t.Object({ url: t.String() }) }
+      )
+      .post(
+        '/decode',
+        ({ body, error }) => {
+          const decodedURL = shortenedURLController.decode(body.url);
+
+          // Return 404 if the URL is not found
+          if (!decodedURL) {
+            return error(404, 'URL not found');
+          }
+
+          // Return the decoded URL data as JSON
+          return { ...decodedURL };
+        },
+        { body: t.Object({ url: t.String() }) }
+      )
+      .get('/statistic/:encodedURLPath', ({ params, error }) => {
+        const shortenedURL = shortenedURLController.getStats(
+          params.encodedURLPath
+        );
+
+        // Return 404 if the URL is not found
+        if (!shortenedURL) {
+          return error(404, 'URL not found');
+        }
+
+        // Return the shortened URL data as JSON
+        return { ...shortenedURL };
+      })
+      .get('/list', ({ error }) => {
+        const shortenedURLs = shortenedURLController.list();
+
+        // Return 404 if the URLs are not found
+        if (!shortenedURLs) {
+          return error(404, 'URL not found');
+        }
+
+        // Return the shortened URL data as JSON
+        return [...shortenedURLs];
+      });
+  })
+  .get('/:encodedURLPath', ({ params, error, redirect }) => {
+    const shortenedURL = shortenedURLController.getStats(params.encodedURLPath);
+
+    // Return 404 if the URL is not found
+    if (!shortenedURL) {
+      return error(404, 'URL not found');
+    }
+
+    // Redirect to the original URL
+    return redirect(shortenedURL.originalURL);
+  });
 
 export default apiRoutes;
