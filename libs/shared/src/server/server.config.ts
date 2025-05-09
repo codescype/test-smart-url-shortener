98910e4f0ccd14cb397810282330f5f85d9ad32e
Codescype <engineer.codescype@gmail.com>
2025-05-09 02:13:52 +0100

    feat: add baseURL to the server config

--- libs/shared/src/server/server.config.ts
+++ libs/shared/src/server/server.config.ts
@@ -3,3 +3,5 @@ export const hostname = process.env.SERVER_APP_HOST ?? '127.0.0.1';
 
 // Set the port for the server to listen on
 export const port = process.env.SERVER_APP_PORT ?? 3001;
+
+export const baseURL = `http://${hostname}:${port}`;
