267b1e4b53b76baf11012d57d1a1d395ff00b642
Codescype <engineer.codescype@gmail.com>
2025-07-11 16:08:14 +0100

    feat(server-app): use decorator to provide shortenedURLController instance for its routes

--- apps/server/src/routes/api.ts
+++ apps/server/src/routes/api.ts
@@ -3,10 +3,8 @@ import { Elysia, t } from 'elysia';
 import { ShortenedURLController } from './../controllers/ShortenedURL.controller';
 import { apiPath, shortenedUrlAPISearchPath } from '@tsus/shared-lib';
 
-// Instantiate the URLShortenerService
-const shortenedURLController = new ShortenedURLController();
-
 const apiRoutes = new Elysia()
+  // Add an error handler to catch errors on the route
   .onError(({ code, error }) => {
     console.error('Error:', error);
 
@@ -15,11 +13,17 @@ const apiRoutes = new Elysia()
       message: error,
     };
   })
+
+  // Add a ShortenedURLController instance to the API routes
+  .decorate('shortenedURLController', new ShortenedURLController())
+
+  // Define routes for the routes group
   .group(apiPath, (app) => {
     return app
+      // Define encode route
       .post(
         '/encode',
-        ({ body, error }) => {
+        ({ body, error, shortenedURLController }) => {
           const shortenedURL = shortenedURLController.encode(body.url);
 
           // Return 404 if the URL is not found
@@ -31,15 +35,16 @@ const apiRoutes = new Elysia()
           return { ...shortenedURL };
         },
         { body: t.Object({ url: t.String() }) }
-      )
+    )
+      
+      // Define decode route
       .post(
         '/decode',
-        ({ body, error }) => {
+        ({ body, error, shortenedURLController }) => {
           const decodedURL = shortenedURLController.decode(body.url);
 
           // Return 404 if the URL is not found
           if (!decodedURL) {
-            ;
             return error(404, 'URL not found');
           }
 
@@ -47,36 +52,42 @@ const apiRoutes = new Elysia()
           return { ...decodedURL };
         },
         { body: t.Object({ url: t.String() }) }
-      )
-      .get(`${shortenedUrlAPISearchPath}/:encodedURLPath`, ({ params, error }) => {
-        const shortenedURL = shortenedURLController.getStats(
-          params.encodedURLPath
-        );
-
-        // Return 404 if the URL is not found
-        if (!shortenedURL) {
-          return error(404, 'URL not found');
-        }
-
-        // Return the shortened URL data as JSON
-        return { ...shortenedURL };
-      })
+    )
+      
+      // Define search routes
       .get(
-        '/list',
-        ({ error }) => {
-          const shortenedURLs = shortenedURLController.list();
+        `${shortenedUrlAPISearchPath}/:encodedURLPath`,
+        ({ params, error, shortenedURLController }) => {
+          const shortenedURL = shortenedURLController.getStats(
+            params.encodedURLPath
+          );
 
-          // Return 404 if the URLs are not found
-          if (!shortenedURLs) {
+          // Return 404 if the URL is not found
+          if (!shortenedURL) {
             return error(404, 'URL not found');
           }
 
           // Return the shortened URL data as JSON
-          return [...shortenedURLs];
+          return { ...shortenedURL };
         }
-      );
+    )
+      
+      // Define list route
+      .get('/list', ({ error, shortenedURLController }) => {
+        const shortenedURLs = shortenedURLController.list();
+
+        // Return 404 if the URLs are not found
+        if (!shortenedURLs) {
+          return error(404, 'URL not found');
+        }
+
+        // Return the shortened URL data as JSON
+        return [...shortenedURLs];
+      });
   })
-  .get('/:encodedURLPath', ({ params, error }) => {
+
+  // Define stats route
+  .get('/:encodedURLPath', ({ params, error, shortenedURLController }) => {
     const shortenedURL = shortenedURLController.getStats(params.encodedURLPath);
 
     // Return 404 if the URL is not found
